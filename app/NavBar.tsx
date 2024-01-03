a27301e70a1f9aa8ae923d96299c54e33c3770dc
Boualam3 <boualam.bourahla@gmail.com>
2024-01-03 15:40:45 +0100

    Refactore the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -22,12 +22,6 @@ import {
 import { Spinner } from "./_components"
 
 const NavBar = () => {
-  const currentPathname = usePathname()
-  const { status, data: session } = useSession()
-  const links = [
-    { href: "/issues/list", label: "Issues" },
-    { href: "/dashboard", label: "Dashboard" },
-  ]
   return (
     <div className="mb-5 px-5 py-3 shadow-sm">
       <Container>
@@ -36,56 +30,83 @@ const NavBar = () => {
             <Link href="/">
               <AiFillBug />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    href={link.href}
-                    className={classnames({
-                      "text-zinc-900": link.href === currentPathname,
-                      "text-zinc-500": link.href !== currentPathname,
-                      "hover:text-zinc-800 transition-colors": true,
-                    })}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user?.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Text size="2">
-                      <Link href="/api/auth/signout">Log out</Link>
-                    </Text>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "loading" && <Spinner />}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Log in</Link>
-            )}
-          </Box>
+          {/*  */}
+          <AuthStatus />
         </Flex>
       </Container>
     </div>
   )
+}
+
+function NavLinks() {
+  const currentPathname = usePathname()
+
+  const links = [
+    { href: "/issues/list", label: "Issues" },
+    { href: "/dashboard", label: "Dashboard" },
+  ]
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            href={link.href}
+            className={classnames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currentPathname,
+            })}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+function AuthStatus() {
+  const { status, data: session } = useSession()
+  if (status === "loading") return <Spinner />
+  if (status === "authenticated") {
+    return (
+      <Box>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              src={session.user!.image!}
+              fallback="?"
+              size="2"
+              radius="full"
+              className="cursor-pointer"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2">{session.user?.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Text size="2">
+                <Link className="nav-link" href="/api/auth/signout">
+                  Log out
+                </Link>
+              </Text>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </Box>
+    )
+  }
+
+  if (status === "unauthenticated") {
+    return (
+      <Box>
+        <Link href="/api/auth/signin">Log in</Link>
+      </Box>
+    )
+  }
 }
 
 export default NavBar
