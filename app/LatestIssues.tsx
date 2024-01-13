da68ad07a28bccdf03e334328578c3ac4c3099c7
Boualam3 <boualam.bourahla@gmail.com>
2024-01-13 14:53:39 +0100

    build the IssueSummary compoenent

--- app/LatestIssues.tsx
+++ app/LatestIssues.tsx
@@ -1,6 +1,5 @@
 import prisma from "@/prisma/client"
 import { Avatar, Card, Flex, Heading, Table } from "@radix-ui/themes"
-import React from "react"
 import { IssueStatusBadge } from "./_components"
 import Link from "next/link"
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 import Image from "next/image"
 import Pagination from "./_components/Pagination"
 import LatestIssues from "./LatestIssues"
-
-export default function Home() {
-  return <LatestIssues />
+import IssueSummary from "./IssueSummary"
+import prisma from "@/prisma/client"
+export default async function Home() {
+  const openedIssue = await prisma.issue.count({ where: { status: "OPEN" } })
+  const progressedIssue = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  })
+  const closedIssue = await prisma.issue.count({ where: { status: "CLOSED" } })
+  return (
+    <IssueSummary
+      open={openedIssue}
+      inProgress={progressedIssue}
+      closed={closedIssue}
+    />
+  )
 }
